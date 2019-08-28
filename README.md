66eebff2708e50f69b7110a6255e1b31cc6f85af
27Cobalter <kontyanzhihg@gmail.com>
2019-08-29 04:16:56 +0900

    [add]とりあえず作ったやつ

---
+++ README.md
@@ -0,0 +1,40 @@
+# vrc_log_viewer
+## これは何
+- VRCのログを見たいときに使うやつ
+
+## 依存パッケージ
+- pyyaml
+```
+$ pip install pyyaml
+```
+
+## 実行方法
+### pythonで実行
+- vrc_log_viewer.pyと同じ階層に設定ファイル`config.yml`を配置して以下のコマンドを実行
+```
+$ python vrc_log_viewer.py
+```
+### 実行ファイル生成
+```
+$ pyinstaller.exe vrc_log_viewer.py [-F -w]
+  オプションはお好みで
+  -F : ファイルを1つにまとめる
+  -w : 実行時にウィンドウを表示しない(タスクマネージャーからkill)
+```
+dist以下に実行ファイルが生成されるのでexeファイルと同じ階層に設定ファイル`config.yml`を配置してexeファイルを実行
+
+## 設定ファイル
+- 例
+```notice.yml
+# 見るログファイルの名前(指定無しの場合作成日時が一番新しいファイルを開く)
+logfile: ""
+# trueの場合起動する前に書き込まれたログも出力
+past: true
+# 出力するログを正規表現で表記(グループ化してある部分を出力)
+# 2019.08.28 23:06:46 Log        -  [NetworkManager] OnPlayerJoined 27Cobalter が
+# 2019.08.28 23:06:46 OnPlayerJoined 27Cobalter のように出力される
+reg:
+  - '([0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*?\[NetworkManager\] (OnPlayerJoined .*)'
+  - '([0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*?\[NetworkManager\] (OnPlayerLeft .*)'
+  - '([0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*?\[RoomManager\] (Joining wrld.*)'
+```
---
+++ vrc_log_viewer.py
@@ -0,0 +1,49 @@
+import time
+import glob
+import os
+import re
+import yaml
+
+
+def tail(thefile, past):
+    if not past:
+        thefile.seek(0, 2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.5)
+            continue
+        line = line.rstrip("\n").rstrip("\r")
+        if line != "":
+            yield line
+
+
+if __name__ == "__main__":
+    with open("config.yml", "r") as config:
+        conf = yaml.load(config, Loader=yaml.SafeLoader)
+    print("load config")
+    reg = []
+    for pattern in conf["reg"]:
+        print("  " + pattern)
+        reg.append(re.compile(pattern))
+
+    vrcdir = os.environ["USERPROFILE"] + "\\AppData\\LocalLow\\VRChat\\VRChat\\"
+    logfile = vrcdir + conf["logfile"]
+    if logfile == vrcdir:
+        logfiles = glob.glob(vrcdir + "output_log_*.txt")
+        logfiles.sort(key=os.path.getctime, reverse=True)
+        logfile = logfiles[0]
+
+    with open(logfile, "r", encoding="utf-8") as f:
+        print("open logfile : ", logfile)
+        loglines = tail(f, conf["past"])
+
+        for line in loglines:
+            for pattern in reg:
+                match = pattern.match(line)
+                if not match:
+                    continue
+                message = ""
+                for group in match.groups():
+                    message = message + group + " "
+                print(message)
